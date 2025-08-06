7b0cac0353d66554d0c9cc5e63ffdc2983303d33
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2025-08-06 15:27:16 +0100

    start refactoring parameter sweep

--- ragone_parameters.py
+++ ragone_parameters.py
@@ -1,8 +1,7 @@
 import pybamm
 import numpy as np
-import gc
-from util import compute_ragone, plot_ragone
-from ragone import RagoneSimulation, RagonePlot
+from ragone import RagoneSimulation, RagonePlot, get_parameter_values, get_options
+from pathlib import Path
 
 options, tag = get_options(SEI=True, plating=True, lam=True)
 
@@ -10,77 +9,27 @@ model = pybamm.lithium_ion.DFN(
     options=options,
 )
 
-
-def j_p0(c_e, c_s_surf, c_s_max, T):
-    m_ref = pybamm.Parameter(
-        "Positive electrode reaction rate"
-    )  # (A/m2)(m3/mol)**1.5 - includes ref concentrations
-    E_r = 17800
-    arrhenius = np.exp(E_r / pybamm.constants.R * (1 / 298.15 - 1 / T))
-
-    return m_ref * arrhenius * c_e**0.5 * c_s_surf**0.5 * (c_s_max - c_s_surf) ** 0.5
-
-
-def j_n0(c_e, c_s_surf, c_s_max, T):
-    m_ref = pybamm.Parameter(
-        "Negative electrode reaction rate"
-    )  # (A/m2)(m3/mol)**1.5 - includes ref concentrations
-    E_r = 35000
-    arrhenius = np.exp(E_r / pybamm.constants.R * (1 / 298.15 - 1 / T))
-
-    return m_ref * arrhenius * c_e**0.5 * c_s_surf**0.5 * (c_s_max - c_s_surf) ** 0.5
-
-
-parameter_values = pybamm.ParameterValues("OKane2022")
-Chen2020 = pybamm.ParameterValues("Chen2020")
-parameter_values["Negative electrode OCP [V]"] = Chen2020["Negative electrode OCP [V]"]
-parameter_values["Electrolyte diffusivity [m2.s-1]"] = 1.7694e-10
-parameter_values["Electrolyte conductivity [S.m-1]"] = 0.9487
-parameter_values["Negative electrode exchange-current density [A.m-2]"] = j_n0
-parameter_values["Positive electrode exchange-current density [A.m-2]"] = j_p0
-parameter_values.update(
-    {
-        "Negative electrode reaction rate": 6.48e-7,
-        "Positive electrode reaction rate": 3.42e-6,
-    },
-    check_already_exists=False,
-)
-parameter_values["Negative electrode exchange-current density [A.m-2]"] = j_n0
+parameter_values = get_parameter_values(ageing=False)
 
 volume = parameter_values["Cell volume [m3]"] * 1000
 
+aged_sol = pybamm.load(Path("data") / f"aged_solution{tag}.pkl")
+
 var_pts = {
-    "x_n": 50,
-    "x_s": 20,
-    "x_p": 50,
+    "x_n": 30,
+    "x_s": 30,
+    "x_p": 30,
     "r_n": 20,
     "r_p": 20,
 }
 
-parameter_sweeps = {
-    # "Negative electrode porosity": np.linspace(0.25, 0.05, 5),
-    # "Negative electrode active material volume fraction": np.linspace(0.75, 0.35, 5),
-    # "Positive electrode porosity": np.linspace(0.335, 0.135, 5),
-    # "Positive electrode active material volume fraction": np.linspace(0.665, 0.265, 5),
-    "Negative electrode reaction rate": np.logspace(
-        np.log10(6.48e-7), np.log10(6.48e-9), 5
-    ),
-    # "Positive electrode reaction rate": np.logspace(np.log10(3.42e-6), np.log10(3.42e-8), 5),
-}
+step = 100
 
-filename_extension = {
-    "Negative electrode porosity": "eps_n",
-    "Negative electrode active material volume fraction": "amvf_n",
-    "Positive electrode porosity": "eps_p",
-    "Positive electrode active material volume fraction": "amvf_p",
-    "Negative electrode reaction rate": "j0_n",
-    "Positive electrode reaction rate": "j0_p",
-}
+ageing_solutions = [aged_sol.all_first_states[0]] + aged_sol.all_first_states[
+    step - 1 :: step
+]
 
-value_ranges = {
-    "power": np.logspace(np.log10(0.5), np.log10(100), 50),
-    "current": np.logspace(np.log10(0.1), np.log10(30), 50),
-}
+
 
 solver = pybamm.IDAKLUSolver(
     output_variables=["Voltage [V]"],
