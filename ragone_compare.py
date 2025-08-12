34a198747c482655d452636f657f3d7f5456f9a2
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2025-08-12 16:59:51 +0100

    compare modes script

--- ragone_compare.py
+++ ragone_compare.py
@@ -1,92 +1,105 @@
 import pybamm
 import numpy as np
-import gc
-from util import compute_ragone, plot_ragone
+from ragone import RagoneSimulation, RagonePlot, get_parameter_values, get_options
 
-model = pybamm.lithium_ion.DFN(
-    options={
-        "SEI": "none",
-        "calculate discharge energy": "true",
-    }
-)
+# options, tag = get_options(SEI=False, plating=False, lam=False)
 
-parameter_values = pybamm.ParameterValues("OKane2022")
-Chen2020 = pybamm.ParameterValues("Chen2020")
-parameter_values["Negative electrode OCP [V]"] = Chen2020["Negative electrode OCP [V]"]
-parameter_values["Electrolyte diffusivity [m2.s-1]"] = 1.7694e-10
-parameter_values["Electrolyte conductivity [S.m-1]"] = 0.9487
+model = pybamm.lithium_ion.DFN()
 
-volume = parameter_values["Cell volume [m3]"] * 1000
-
-solver = pybamm.IDAKLUSolver()
+parameter_values = get_parameter_values(ageing=False)
 
 var_pts = {
-    "x_n": 50,
-    "x_s": 20,
-    "x_p": 50,
+    "x_n": 30,
+    "x_s": 30,
+    "x_p": 30,
     "r_n": 20,
     "r_p": 20,
 }
-experiment_setup = pybamm.Experiment(
+
+solver = pybamm.IDAKLUSolver(rtol=1e-8, atol=1e-10)
+
+# Obtain discharged and charged solutions
+experiment_dch = pybamm.Experiment(
     [
         "Discharge at C/10 until 2.5V",
         "Hold at 2.5V until C/50",
     ]
 )
-
-aged_sol = pybamm.load("solution.pkl")
-model.set_initial_conditions_from(aged_sol.all_first_states[-1], inplace=True)
-
-sim_setup = pybamm.Simulation(
+sim_dch = pybamm.Simulation(
     model,
     parameter_values=parameter_values,
-    experiment=experiment_setup,
+    experiment=experiment_dch,
+    var_pts=var_pts,
     solver=solver,
 )
-sol_setup = sim_setup.solve()
+sol_dch = sim_dch.solve()
 
-model_discharged = model.set_initial_conditions_from(
-    sol_setup.last_state, inplace=False
+experiment_ch = pybamm.Experiment(
+    [
+        "Charge at C/10 until 4.2V",
+        "Hold at 4.2V until C/50",
+    ]
 )
+sim_ch = pybamm.Simulation(
+    model,
+    parameter_values=parameter_values,
+    experiment=experiment_ch,
+    var_pts=var_pts,
+    solver=solver,
+)
+sol_ch = sim_ch.solve()
 
-# modes = ["current"]
-modes = ["power", "current"]
-models = [model, model_discharged]
-directions = ["discharge", "charge"]
-outputs = []
-inputs = []
 
-ref_voltage = (
-    parameter_values["Upper voltage cut-off [V]"]
-    + parameter_values["Lower voltage cut-off [V]"]
-) / 2
-
-power_range = np.logspace(np.log10(0.1), np.log10(150), 50)
-value_ranges = [
-    power_range,
-    [i / ref_voltage for i in power_range],
-]
+value_ranges = {
+    "power": np.logspace(np.log10(0.5), np.log10(100), 5),
+    "current": np.logspace(np.log10(0.1), np.log10(30), 5),
+}
 
 labels = []
+solutions = []
 
-i = 0
-for value_range, mode in zip(value_ranges, modes):
-    for model, direction in zip(models, directions):
-        i += 1
-        print(
-            f"Running Ragone plot for solution {i} of {len(value_ranges) * len(models)}"
-        )
-        output, input = compute_ragone(
-            model,
-            parameter_values,
-            value_range=value_range,
-            mode=mode,
-            direction=direction,
-            convert_to_watts=True,
-        )
-        outputs.append(output)
-        inputs.append(input)
+for mode, value_range in value_ranges.items():
+    print(f"Starting Ragone plots - {mode} mode")
+    for direction, sol_init in [("charge", sol_dch), ("discharge", sol_ch)]:
+        print(f"Running Ragone plot for direction: {direction}")
         labels.append(f"{mode} - {direction}")
 
-fig = plot_ragone(inputs, outputs, mode="power", labels=labels)
-fig.savefig("./figures/" + f"ragone_compare_modes_directions.png", dpi=300)
+        new_model = model.set_initial_conditions_from(sol_init.last_state, inplace=False)
+
+        sim = RagoneSimulation(
+            new_model,
+            parameter_values=parameter_values,
+            value_range=value_range,
+            var_pts=var_pts,
+            solver=solver,
+            mode=mode,
+            direction=direction,
+            convert_to_watts=(mode == "current"),
+        )
+
+        sol = sim.solve()
+
+        solutions.append(sol)
+
+
+
+plt = RagonePlot(solutions, labels=labels)
+fig, ax = plt.plot(show_plot=False)
+
+# Set axes manually to match other figures (current plots "overflow")
+solution = solutions[1]
+ax.set_xlim(solution.min_input, 80)
+ax.set_ylim(0.1 * solution.max_output, 1.1 * solution.max_output)
+
+fig.savefig(
+    "./figures/" + "ragone_compare_modes_directions.png",
+    dpi=300,
+)
+
+ax.set_xscale("linear")
+ax.set_yscale("linear")
+
+fig.savefig(
+    "./figures/" + "ragone_compare_modes_directions_linear.png",
+    dpi=300,
+)
