ead811f7518c7d8b5576a614ff9c719d91f12e7f
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2026-06-19 09:16:09 +0100

    update script names in run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,4 +1,4 @@
-sbatch slurm/ragone_ageing_parallel.slurm
+sbatch slurm/ragone_ageing.slurm
 sbatch slurm/ragone_compare.slurm
 sbatch slurm/ragone_parameters.slurm
-sbatch slurm/rate_capability.slurm
+sbatch slurm/rpt.slurm
